6f86ffb67e7c384277803974f9b8c2393a9273cd
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2022-07-17 12:15:11 -0300

    initial commit

---
+++ src/main/java/br/com/coding/puma/PumaApplication.java
@@ -0,0 +1,13 @@
+package br.com.coding.puma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coding/puma/PumaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coding.puma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
