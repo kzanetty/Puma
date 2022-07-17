129f628ae0e9f37c7c160a6bbf867cb8aefca7a3
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2022-07-17 13:02:41 -0300

    feat: create test endpoint

---
+++ src/main/java/br/com/coding/puma/TestController.java
@@ -0,0 +1,18 @@
+package br.com.coding.puma;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    ResponseEntity<String> test(@RequestParam @NotBlank String clientName) {
+        var body = "my name is PUMA and yours is %s".formatted(clientName);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+}
